48afd2ba0f80727ab570023ddaa5fd6a196fd044
Claudio Benfatto <claudio.benfatto@gmail.com>
2017-06-15 13:54:12 +0100

    add web server for json details and pprof

--- main.go
+++ main.go
@@ -1,17 +1,26 @@
 package main
 
 import (
+	"log"
 	"time"
+	"net/http"
+	_ "net/http/pprof"
+	"encoding/json"
 
 	"github.com/SpringerPE/graphite-du-report/reporter"
 )
 
-func main() {
+func getDetails(w http.ResponseWriter, r *http.Request) {
 	fetcher := reporter.NewDataFetcher(120*time.Second, 3)
 	response := reporter.GetDetails([]string{"127.0.0.1"}, "", fetcher)
 	root := &reporter.Node{Name: "root", Children: map[string]*reporter.Node{}}
 	reporter.ConstructTree(root, response)
 	reporter.UpdateSize(root)
-	//fmt.Printf("%v", *root)
-	reporter.Visit("", root)
+	w.Header().Set("Content-Type", "application/json")
+	json.NewEncoder(w).Encode(&root)
+}
+
+func main() {
+	http.HandleFunc("/", getDetails)
+	log.Println(http.ListenAndServe("localhost:6060", nil))
 }
