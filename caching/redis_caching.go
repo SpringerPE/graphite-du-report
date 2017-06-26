fd03a37f96585e526236313a78b8f6b87a6fd012
Claudio Benfatto <claudio.benfatto@gmail.com>
2017-06-26 16:00:49 +0100

    improve error checking

--- caching/redis_caching.go
+++ caching/redis_caching.go
@@ -13,15 +13,10 @@ type RedisCaching struct {
 func (r *RedisCaching) SetNode(node *Node) error {
 	conn := r.Pool.Get()
 	defer conn.Close()
-	_, err := conn.Do("HMSET",
-		node.Name,
-		"leaf", node.Leaf,
-		"size", node.Size,
-	)
+
+	_, err := conn.Do("HMSET", node.Name, "leaf", node.Leaf, "size", node.Size)
 	for _, child := range node.Children {
-		_, err := conn.Do("SADD",
-			node.Name+":children",
-			child)
+		_, err := conn.Do("SADD", node.Name+":children", child)
 		if err != nil {
 			fmt.Println(err)
 		}
@@ -42,35 +37,32 @@ func (r *RedisCaching) AddChild(node *Node, child string) error {
 
 func (r *RedisCaching) GetNode(key string) (*Node, error) {
 
-	node := &Node{Name: key}
-
 	conn := r.Pool.Get()
 	defer conn.Close()
-	reply, err := redis.Values(conn.Do("HGETALL", key))
 
-	if len(reply) == 0 {
-		return nil, nil
-	}
+	node := &Node{Name: key}
+
+	reply, err := redis.Values(conn.Do("HGETALL", key))
 
 	if err != nil {
 		fmt.Printf("error:%v , reply:%v", err, reply)
+	}
+
+	if len(reply) == 0 {
+		return nil, nil
 	}
 
 	if err := redis.ScanStruct(reply, node); err != nil {
 		fmt.Println(err)
 	}
 
-	fmt.Printf("REPLY %#v\n", node)
-	if node != nil {
-		reply, err = redis.Values(conn.Do("SMEMBERS", key+":children"))
-		var children []string
-		if err := redis.ScanSlice(reply, &children); err != nil {
-			fmt.Println(err)
-		}
-
-		node.Children = children
+	reply, err = redis.Values(conn.Do("SMEMBERS", key+":children"))
+	var children []string
+	if err := redis.ScanSlice(reply, &children); err != nil {
+		fmt.Println(err)
 	}
-	node.Name = key
+
+	node.Children = children
 	return node, err
 }
 
