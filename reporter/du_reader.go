0dec02bbf2ae118aed5466024e0d8ff0a87fa8b3
Claudio Benfatto <claudio.benfatto@gmail.com>
2017-07-17 14:50:53 +0100

    remove unused methods from reader

--- reporter/du_reader.go
+++ reporter/du_reader.go
@@ -1,12 +1,12 @@
 package reporter
 
 import (
-	"fmt"
+	//"fmt"
 	_ "net/http/pprof"
-	"strings"
+	//"strings"
 
 	"github.com/SpringerPE/graphite-du-report/caching"
-	"github.com/SpringerPE/graphite-du-report/logging"
+	//"github.com/SpringerPE/graphite-du-report/logging"
 )
 
 type TreeReader struct {
@@ -27,23 +27,6 @@ func (tree *TreeReader) ReadNode(key string) (*caching.Node, error) {
 
 func (tree *TreeReader) ReadFlameMap() ([]string, error) {
 	return tree.reader.ReadFlameMap()
-}
-
-//Calculates the disk usage in terms of number of files contained
-func (tree *TreeReader) Visit(root *caching.Node, doc *[]string) {
-	//if it is a leaf its size is already given
-	if root.Leaf || len(root.Children) == 0 {
-		*doc = append(*doc, fmt.Sprintf("%s %d", strings.Replace(root.Name, ".", ";", -1), root.Size))
-		return
-	}
-
-	for _, child := range root.Children {
-		node, err := tree.ReadNode(root.Name + "." + child)
-		if err != nil {
-			logging.LogError("cannot read node", err)
-		}
-		tree.Visit(node, doc)
-	}
 }
 
 func (tree *TreeReader) GetNodeSize(path string) (int64, error) {
