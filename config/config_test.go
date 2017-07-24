4d3d6a95b11601034fbd6a9d63b60dad29fa5b0d
Claudio Benfatto <claudio.benfatto@gmail.com>
2017-07-24 11:25:48 +0100

    improve test naming and descriptions

--- config/config_test.go
+++ config/config_test.go
@@ -9,15 +9,15 @@ import (
 
 var _ = Describe("Config", func() {
 
-	Describe("Default configuration", func() {
-		It("default config contains the expected parameters", func() {
+	Describe("given a default configuration object", func() {
+		It("should contain the expected parameters", func() {
 			c := config.DefaultUpdaterConfig()
 			Expect(c.Servers).To(Equal([]string{"127.0.0.1:8080"}))
 		})
 	})
 
-	Describe("Given a string consisting of comma separated server strings", func() {
-		It("returns a list of the servers", func() {
+	Describe("given a string consisting of comma separated server strings", func() {
+		It("should return a list of servers", func() {
 			s := "example.host:8080, 127.0.0.1:7777 , my.host:80"
 			sList := config.ParseServerList(s)
 			Expect(sList).To(Equal([]string{"example.host:8080", "127.0.0.1:7777", "my.host:80"}))
