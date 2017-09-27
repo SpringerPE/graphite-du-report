426f92c1e593cfc792b9720eac383e84ba8b3764
Claudio Benfatto <claudio.benfatto@gmail.com>
2017-09-27 11:49:22 +0100

    make renderer endpoint in js function parametric

--- assets/visualiser/static/scripts/functions.js
+++ assets/visualiser/static/scripts/functions.js
@@ -1,18 +1,25 @@
-$(document).ready(function(){
+function pathJoin(parts, sep){
+	var separator = sep || '/';
+	var replace   = new RegExp(separator+'{1,}', 'g');
+	return parts.join(separator).replace(replace, separator);
+}
+
+function render_flame(renderer_path){
 	$.ajax({
-	    url: location.origin + "/renderer/flame",
-	    async: true,
-	    dataType: "html",
+		url: location.origin + pathJoin(["/", renderer_path, "flame"]),
+		async: true,
+		dataType: "html",
 
 		beforeSend: function(xhr) {
-			/*showing  a div with spinning image */
-      		$('#loaderImg').show();
-			xhr.setRequestHeader('X-Cache-Enabled', 'true');
-		},
-	    success: function(data){
-       		/*  simply hide the image */    
-       		$('#loaderImg').hide();
+		/*showing  a div with spinning image */
+		$('#loaderImg').show();
+		xhr.setRequestHeader('X-Cache-Enabled', 'true');
+	},
+
+		success: function(data){
+			/*  simply hide the image */
+			$('#loaderImg').hide();
 			$('#svgdiv').append(data);
-	    }
+		}
 	});
-});
+}
